28b638a969887a4027ffeba3976ff642f51ee129
HaiderMalik <haidermalik504@gmail.com>
2017-01-15 12:38:50 +0500

    Setting Up Route Configuration with About Contact and products pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+const appRoutes: Routes = [
+  { path: 'products', component: ProductsComponent },
+  { path: 'contact', component: ContactComponent},
+  { path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ProductsComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
